a58cc442d856ccb2f4b8f5472a138820628dfd37
hintofbasil <hintofbasil@users.noreply.github.com>
2016-12-10 18:34:08 +0000

    Added fake handling on lets go button click

--- app/src/main/java/com/github/hintofbasil/standingalone/MainActivity.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/MainActivity.java
@@ -1,9 +1,15 @@
 package com.github.hintofbasil.standingalone;
 
+import android.util.Log;
+import android.view.View;
+
 public class MainActivity extends BaseActivity {
 
     public MainActivity() {
         super(R.drawable.the_story);
     }
 
+    public void handleLetsGoButtonClick(View view) {
+        Log.e("handleLetsGoButtonClick", "This method has not yet been implemented");
+    }
 }
