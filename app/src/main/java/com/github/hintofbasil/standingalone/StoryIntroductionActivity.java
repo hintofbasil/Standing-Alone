fadd7ab8e47810b1dace11ce19a4352e9329d9a3
hintofbasil <hintofbasil@users.noreply.github.com>
2016-12-11 16:55:09 +0000

    Added progressing of introduction story

--- app/src/main/java/com/github/hintofbasil/standingalone/StoryIntroductionActivity.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/StoryIntroductionActivity.java
@@ -1,11 +1,75 @@
 package com.github.hintofbasil.standingalone;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class StoryIntroductionActivity extends BaseActivity {
 
+    private ViewFlipper backgroundImageFlipper, introductionStoryFlipper;
+
+    private ImageView leftButton, rightButton;
+
+    private int currentPage;
+    private int maxPage;
+
     public StoryIntroductionActivity() {
         super(R.drawable.the_story, R.layout.activity_story_introduction);
+        currentPage = 0;
+        maxPage = 4;
+    }
+
+    public void onLeftNavigationClicked(View view) {
+        currentPage--;
+        getBackgroundImageFlipper().showPrevious();
+        updateDisplayForPage();
+    }
+
+    public void onRightNavigationClicked(View view) {
+        currentPage++;
+        getBackgroundImageFlipper().showNext();
+        updateDisplayForPage();
+    }
+
+    private void updateDisplayForPage() {
+        if (currentPage == 0) {
+            getLeftButton().setVisibility(View.INVISIBLE);
+        } else {
+            getLeftButton().setVisibility(View.VISIBLE);
+        }
+
+        if (currentPage == maxPage) {
+            getRightButton().setVisibility(View.INVISIBLE);
+        } else {
+            getRightButton().setVisibility(View.VISIBLE);
+        }
+    }
+
+    private ViewFlipper getBackgroundImageFlipper() {
+        if (backgroundImageFlipper == null) {
+            backgroundImageFlipper = (ViewFlipper) findViewById(R.id.background_image_flipper);
+        }
+        return backgroundImageFlipper;
+    }
+
+    private ImageView getLeftButton() {
+        if (leftButton == null) {
+            leftButton = (ImageView) findViewById(R.id.introduction_left_button);
+        }
+        return leftButton;
+    }
+
+    private ImageView getRightButton() {
+        if (rightButton == null) {
+            rightButton = (ImageView) findViewById(R.id.introduction_right_button);
+        }
+        return rightButton;
+    }
+
+    public ViewFlipper getIntroductionStoryFlipper() {
+        if (introductionStoryFlipper == null) {
+            introductionStoryFlipper = (ViewFlipper) findViewById(R.id.introduction_story_flipper);
+        }
+        return introductionStoryFlipper;
     }
 }
