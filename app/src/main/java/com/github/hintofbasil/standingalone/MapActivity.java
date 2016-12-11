f7b230158d9766b03a3eccae949e6e54290df83c
hintofbasil <hintofbasil@users.noreply.github.com>
2016-12-11 19:07:42 +0000

    Added basic outline for map page

---
+++ app/src/main/java/com/github/hintofbasil/standingalone/MapActivity.java
@@ -0,0 +1,11 @@
+package com.github.hintofbasil.standingalone;
+
+/**
+ * Created by will on 11/12/16.
+ */
+public class MapActivity extends BaseActivity {
+
+    public MapActivity() {
+        super(R.drawable.map_title, R.layout.activity_map);
+    }
+}
