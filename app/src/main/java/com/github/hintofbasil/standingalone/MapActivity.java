89b9ce19a7616de7010f48b357ba9e0051115dbc
hintofbasil <hintofbasil@users.noreply.github.com>
2016-12-16 22:47:31 +0000

    Added implementation to cheat button

--- app/src/main/java/com/github/hintofbasil/standingalone/MapActivity.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/MapActivity.java
@@ -1,5 +1,7 @@
 package com.github.hintofbasil.standingalone;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +11,9 @@ import android.widget.ImageView;
 /**
  * Created by will on 11/12/16.
  */
-public class MapActivity extends BaseActivity {
+public class MapActivity extends BaseActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences sharedPreferences;
 
     public MapActivity() {
         super(R.drawable.map_title, R.layout.activity_map);
@@ -25,9 +29,25 @@ public class MapActivity extends BaseActivity {
             Log.d("LocationFoundCheater", "Debug mode enabled, cheat button visible");
             locationFoundCheater.setVisibility(View.VISIBLE);
         }
+        sharedPreferences = getSharedPreferences(getString(R.string.preferences_file_key), Context.MODE_PRIVATE);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     public void onLocationFoundCheaterClickHandler(View view) {
+        String locationsFoundKey = getString(R.string.preferences_locations_found_key);
+        int locationsFoundCount = sharedPreferences.getInt(locationsFoundKey, 0);
+        locationsFoundCount = (locationsFoundCount + 1) % 10;
+        sharedPreferences.edit().putInt(locationsFoundKey, locationsFoundCount).apply();
+    }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 }
