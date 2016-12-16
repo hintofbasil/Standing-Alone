7cbabf0c71fa0ad0b5275eaab42817191a8bc7cd
hintofbasil <hintofbasil@users.noreply.github.com>
2016-12-16 22:57:53 +0000

    Added updating of progress tracking on map

--- app/src/main/java/com/github/hintofbasil/standingalone/MapActivity.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/MapActivity.java
@@ -3,6 +3,7 @@ package com.github.hintofbasil.standingalone;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.ImageView;
 public class MapActivity extends BaseActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private SharedPreferences sharedPreferences;
+    private ImageView[] progressImageViews;
 
     public MapActivity() {
         super(R.drawable.map_title, R.layout.activity_map);
@@ -42,12 +44,45 @@ public class MapActivity extends BaseActivity implements SharedPreferences.OnSha
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        
+        if (key.equals(getString(R.string.preferences_locations_found_key))) {
+            // Loctions found updated
+            int progress = sharedPreferences.getInt(key, 0);
+            updateProgressIndicator(progress);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    private void updateProgressIndicator(int progress) {
+        ImageView[] progressViews = getProgressImageViews();
+        for (int i = 0; i < progressViews.length; i++) {
+            Drawable drawable;
+            if (i < progress) {
+                drawable = getResources().getDrawable(R.drawable.point_selected);
+            } else {
+                drawable = getResources().getDrawable(R.drawable.point_unselected);
+            }
+            progressViews[i].setImageDrawable(drawable);
+        }
+    }
+
+    public ImageView[] getProgressImageViews() {
+        if (progressImageViews == null) {
+            progressImageViews = new ImageView[9];
+            progressImageViews[0] = (ImageView) findViewById(R.id.story_progress_1);
+            progressImageViews[1] = (ImageView) findViewById(R.id.story_progress_2);
+            progressImageViews[2] = (ImageView) findViewById(R.id.story_progress_3);
+            progressImageViews[3] = (ImageView) findViewById(R.id.story_progress_4);
+            progressImageViews[4] = (ImageView) findViewById(R.id.story_progress_5);
+            progressImageViews[5] = (ImageView) findViewById(R.id.story_progress_6);
+            progressImageViews[6] = (ImageView) findViewById(R.id.story_progress_7);
+            progressImageViews[7] = (ImageView) findViewById(R.id.story_progress_8);
+            progressImageViews[8] = (ImageView) findViewById(R.id.story_progress_9);
+        }
+        return progressImageViews;
     }
 }
