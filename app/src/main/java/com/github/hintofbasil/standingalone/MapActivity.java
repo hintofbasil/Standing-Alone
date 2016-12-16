884827b4fa51f717dfd5222ea491fd2955096968
hintofbasil <hintofbasil@users.noreply.github.com>
2016-12-16 22:24:57 +0000

    Added cheat mode to map

--- app/src/main/java/com/github/hintofbasil/standingalone/MapActivity.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/MapActivity.java
@@ -1,5 +1,11 @@
 package com.github.hintofbasil.standingalone;
 
+import android.content.pm.ApplicationInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
 /**
  * Created by will on 11/12/16.
  */
@@ -7,5 +13,21 @@ public class MapActivity extends BaseActivity {
 
     public MapActivity() {
         super(R.drawable.map_title, R.layout.activity_map);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ImageView locationFoundCheater = (ImageView) findViewById(R.id.location_found_cheater);
+        boolean isDebuggable = ( 0 != ( getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE ) );
+        if (isDebuggable) {
+            Log.d("LocationFoundCheater", "Debug mode enabled, cheat button visible");
+            locationFoundCheater.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onLocationFoundCheaterClickHandler(View view) {
+
     }
 }
