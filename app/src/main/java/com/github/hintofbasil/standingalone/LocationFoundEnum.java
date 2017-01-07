77010d7fc2744a995ca6c960a2d63c43647c5341
hintofbasil <hintofbasil@users.noreply.github.com>
2017-01-07 13:45:18 +0000

    Added resources for all LocationFoundEnum

--- app/src/main/java/com/github/hintofbasil/standingalone/LocationFoundEnum.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/LocationFoundEnum.java
@@ -5,8 +5,24 @@ package com.github.hintofbasil.standingalone;
  */
 public enum LocationFoundEnum {
 
-    GLAISTIG(R.drawable.glaistig_title, R.drawable.glaistig,R.drawable.
-            glaistig_background, R.string.location_found_glaistig);
+    GLAISTIG(R.drawable.glaistig_title, R.drawable.glaistig,
+            R.drawable.glaistig_background, R.string.location_found_glaistig),
+    CU_SITH(R.drawable.cu_sith_title, R.drawable.cu_sith,
+            R.drawable.cu_sith_background, R.string.location_found_cu_sith),
+    BLUE_WITCH(R.drawable.blue_witch_title, R.drawable.blue_witch,
+            R.drawable.blue_witch_background, R.string.location_found_blue_witch),
+    MERMAID(R.drawable.mermaid_title, R.drawable.mermaid,
+            R.drawable.mermaid_background, R.string.location_found_mermaid),
+    SELKIE(R.drawable.selkie_title, R.drawable.selkie,
+            R.drawable.selkie_background, R.string.location_found_selkie),
+    KELPIE(R.drawable.kelpie_title, R.drawable.kelpie,
+            R.drawable.introduction_background_2, R.string.location_found_kelpie),
+    CAIT_SITH(R.drawable.cait_sith_title, R.drawable.cait_sith,
+            R.drawable.introduction_background_4, R.string.location_found_cait_sith),
+    BRIAN(R.drawable.brian_title, R.drawable.brian,
+            R.drawable.brian_background, R.string.location_found_brian),
+    TREASURE(R.drawable.treasure_title, R.drawable.treasure,
+            R.drawable.treasure_background, R.string.location_found_treasure);
 
     int titleDrawableId;
     int characterDrawableId;
