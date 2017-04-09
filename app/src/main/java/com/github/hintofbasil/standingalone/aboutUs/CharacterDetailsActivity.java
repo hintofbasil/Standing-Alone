78707fed2eedf16f9950fbdd4be9edeeb6f2a13b
hintofbasil <hintofbasil@github.com>
2017-04-09 15:36:06 +0100

    Added Character details activity

---
+++ app/src/main/java/com/github/hintofbasil/standingalone/aboutUs/CharacterDetailsActivity.java
@@ -0,0 +1,18 @@
+package com.github.hintofbasil.standingalone.aboutUs;
+
+import android.os.Bundle;
+
+import com.github.hintofbasil.standingalone.BaseActivity;
+import com.github.hintofbasil.standingalone.R;
+
+public class CharacterDetailsActivity extends BaseActivity {
+
+    public CharacterDetailsActivity() {
+        super(R.drawable.blank, R.layout.activity_character_details);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
