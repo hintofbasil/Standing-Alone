39f7eadc0aaef41c7294bbe4ccac722d9b95eff6
hintofbasil <hintofbasil@github.com>
2017-04-09 12:44:02 +0100

    Added empty about us activity

---
+++ app/src/main/java/com/github/hintofbasil/standingalone/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.github.hintofbasil.standingalone;
+
+import android.os.Bundle;
+
+public class AboutUsActivity extends BaseActivity {
+
+    public AboutUsActivity() {
+        super(R.drawable.blank, R.layout.activity_about_us);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
