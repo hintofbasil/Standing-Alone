db91414ec2f790b28654c998126999a462276809
hintofbasil <hintofbasil@github.com>
2017-04-09 15:44:28 +0100

    Enabled loading of character details from enum

--- app/src/main/java/com/github/hintofbasil/standingalone/LocationFoundEnum.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/LocationFoundEnum.java
@@ -7,34 +7,44 @@ public enum LocationFoundEnum {
 
     BROWNIE(R.drawable.brownie_title, R.drawable.blank,
             R.drawable.blank, R.array.location_found_brownie,
-            1f, true, false, new int[] {}, R.raw.brownie_found),
+            1f, true, false, new int[] {}, R.raw.brownie_found,
+            R.string.character_details_brownie),
     GLAISTIG(R.drawable.glaistig_title, R.drawable.glaistig,
             R.drawable.glaistig_background, R.array.location_found_glaistig,
-            1f, false, true, new int[] {10000, 6000, 12000, 8000, 8000}, 0),
+            1f, false, true, new int[] {10000, 6000, 12000, 8000, 8000}, 0,
+            0),
     CU_SITH(R.drawable.cu_sith_title, R.drawable.cu_sith,
             R.drawable.cu_sith_background, R.array.location_found_cu_sith,
-            1.6f, true, true, new int[] {10000, 11000, 5000, 8000, 8000, 8000}, R.raw.cu_sith_found),
+            1.6f, true, true, new int[] {10000, 11000, 5000, 8000, 8000, 8000},R.raw.cu_sith_found,
+            0),
     BLUE_WITCH(R.drawable.blue_witch_title, R.drawable.blue_witch,
             R.drawable.blue_witch_background, R.array.location_found_blue_witch,
-            1.5f, true, true, new int[] {8000, 10000, 6000, 8000, 6000, 11000, 10000}, R.raw.blue_witch_found),
+            1.5f, true, true, new int[] {8000, 10000, 6000, 8000, 6000, 11000, 10000}, R.raw.blue_witch_found,
+            0),
     MERMAID(R.drawable.mermaid_title, R.drawable.mermaid,
             R.drawable.mermaid_background, R.array.location_found_mermaid,
-            1.2f, false, true, new int[] {12000, 12000, 6000, 8000, 11000, 5000}, R.raw.mermaid_found),
+            1.2f, false, true, new int[] {12000, 12000, 6000, 8000, 11000, 5000}, R.raw.mermaid_found,
+            0),
     SELKIE(R.drawable.selkie_title, R.drawable.selkie,
             R.drawable.selkie_background, R.array.location_found_selkie,
-            1f, true, true, new int[] {8000, 9000, 13000, 13000, 7000}, R.raw.selkie_found),
+            1f, true, true, new int[] {8000, 9000, 13000, 13000, 7000}, R.raw.selkie_found,
+            0),
     KELPIE(R.drawable.kelpie_title, R.drawable.kelpie,
             R.drawable.introduction_background_2, R.array.location_found_kelpie,
-            2.2f, true, true, new int[] {13000, 9000, 9000, 20000, 7000}, R.raw.kelpie_found),
+            2.2f, true, true, new int[] {13000, 9000, 9000, 20000, 7000}, R.raw.kelpie_found,
+            0),
     CAIT_SITH(R.drawable.cait_sith_title, R.drawable.cait_sith,
             R.drawable.introduction_background_4, R.array.location_found_cait_sith,
-            1f, true, true, new int[] {7000, 9000, 11000, 9000, 8000, 11000}, R.raw.cait_sith_found),
+            1f, true, true, new int[] {7000, 9000, 11000, 9000, 8000, 11000}, R.raw.cait_sith_found,
+            0),
     BRIAN(R.drawable.brian_title, R.drawable.brian,
             R.drawable.brian_background, R.array.location_found_brian,
-            1.6f, false, false, new int[] {15000}, R.raw.brian_found),
+            1.6f, false, false, new int[] {15000}, R.raw.brian_found,
+            0),
     TREASURE(R.drawable.treasure_title, R.drawable.treasure,
             R.drawable.treasure_background, R.array.location_found_treasure,
-            1.7f, true, true, new int[] {10000}, 0);
+            1.7f, true, true, new int[] {10000}, 0,
+            0);
 
     public int titleDrawableId;
     public int characterDrawableId;
@@ -45,12 +55,13 @@ public enum LocationFoundEnum {
     public boolean endWithNoSpeech;
     public int[] textTimings;
     public int foundSoundFile;
+    public int characterDetailsText;
 
     LocationFoundEnum(int titleDrawableId, int characterDrawableId,
                       int backgroundDrawableId, int textStringId,
                       float scale, boolean beginWithBrownie,
                       boolean endWithNoSpeech, int[] textTimings,
-                      int foundSoundFile) {
+                      int foundSoundFile, int characterDetailsText) {
         this.titleDrawableId = titleDrawableId;
         this.characterDrawableId = characterDrawableId;
         this.backgroundDrawableId = backgroundDrawableId;
@@ -60,6 +71,7 @@ public enum LocationFoundEnum {
         this.endWithNoSpeech = endWithNoSpeech;
         this.textTimings = textTimings;
         this.foundSoundFile = foundSoundFile;
+        this.characterDetailsText = characterDetailsText;
     }
 
     public static LocationFoundEnum get(int progress) {
--- app/src/main/java/com/github/hintofbasil/standingalone/aboutUs/CharacterDetailsActivity.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/aboutUs/CharacterDetailsActivity.java
@@ -1,8 +1,11 @@
 package com.github.hintofbasil.standingalone.aboutUs;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.github.hintofbasil.standingalone.BaseActivity;
+import com.github.hintofbasil.standingalone.LocationFoundEnum;
 import com.github.hintofbasil.standingalone.R;
 
 public class CharacterDetailsActivity extends BaseActivity {
@@ -14,5 +17,20 @@ public class CharacterDetailsActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        LocationFoundEnum details = LocationFoundEnum.BROWNIE;
+
+        ImageView titleImage = (ImageView) findViewById(R.id.character_title_image);
+        titleImage.setImageResource(details.titleDrawableId);
+
+        ImageView characterImage = (ImageView) findViewById(R.id.character_image);
+        if (details == LocationFoundEnum.BROWNIE) {
+            characterImage.setImageResource(R.drawable.brownie);
+        } else {
+            characterImage.setImageResource(details.characterDrawableId);
+        }
+
+        TextView characterText = (TextView) findViewById(R.id.character_text);
+
     }
 }
