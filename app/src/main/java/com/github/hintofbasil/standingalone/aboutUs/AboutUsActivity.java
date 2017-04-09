1782f58e12aafa9fdbedfedb5896ad9cc510d034
hintofbasil <hintofbasil@github.com>
2017-04-09 16:34:38 +0100

    Forced about us activity to start scrolled at top

--- app/src/main/java/com/github/hintofbasil/standingalone/aboutUs/AboutUsActivity.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/aboutUs/AboutUsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ScrollView;
 
 import com.github.hintofbasil.standingalone.BaseActivity;
 import com.github.hintofbasil.standingalone.LocationFoundEnum;
@@ -21,7 +22,10 @@ public class AboutUsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ScrollView scrollView = (ScrollView) findViewById(R.id.scroll_view);
+
         AutoHeightGridView characterGridContainer = (AutoHeightGridView) findViewById(R.id.character_grid_container);
+        characterGridContainer.setScrollView(scrollView);
         characterGridContainer.setAdapter(new CharacterGridAdapter(this));
 
         characterGridContainer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
--- app/src/main/java/com/github/hintofbasil/standingalone/aboutUs/AutoHeightGridView.java
+++ app/src/main/java/com/github/hintofbasil/standingalone/aboutUs/AutoHeightGridView.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.GridView;
 import android.widget.ListAdapter;
+import android.widget.ScrollView;
 
 /**
  * Created by will on 09/04/17.
  */
 
 public class AutoHeightGridView extends GridView {
+
+    private ScrollView scrollView;
+
     public AutoHeightGridView(Context context) {
         super(context);
     }
@@ -22,6 +26,10 @@ public class AutoHeightGridView extends GridView {
         super(context, attrs, defStyleAttr);
     }
 
+    public void setScrollView(ScrollView scrollView) {
+        this.scrollView = scrollView;
+    }
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -30,5 +38,7 @@ public class AutoHeightGridView extends GridView {
         int columns = getNumColumns();
         int rows = (int)Math.ceil((float)adapter.getCount() / columns);
         setMeasuredDimension(getMeasuredWidth(), getMeasuredHeight() * rows);
+
+        scrollView.scrollTo(0, 0);
     }
 }
